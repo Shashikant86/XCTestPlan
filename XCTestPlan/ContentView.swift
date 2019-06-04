15a8cffc959c80534ba425c1bb49f5674039c805
Shashikant86 <shashikant.jagtap@icloud.com>
2019-06-04 20:46:05 +0100

    Demo of the Xcode Test Plan Feature

---
+++ XCTestPlan/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  XCTestPlan
+//
+//  Created by Shashikant Jagtap on 04/06/2019.
+//  Copyright © 2019 Shashikant Jagtap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
